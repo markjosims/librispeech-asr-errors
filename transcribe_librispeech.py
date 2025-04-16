a0c0bd4b37b89c20e1cf0cde09017acadbe17e47
Mark Simmons <mjsimmons@ucsd.edu>
2025-04-16 16:37:30 -0700

    use dataset for iteration

--- transcribe_librispeech.py
+++ transcribe_librispeech.py
@@ -1,11 +1,13 @@
 import os
 from transformers import pipeline
 import argparse
-from typing import Tuple, Generator
+from typing import Tuple, Generator, List
 import torch
 import pandas as pd
 import jiwer
 from tqdm import tqdm
+from datasets import Dataset, Audio
+import math
 
 LIBRISPEECH = os.environ.get('LIBRISPEECH')
 DEVICE = 0 if torch.cuda.is_available() else 'cpu'
@@ -36,6 +38,13 @@ def init_parser():
         default=DEVICE,
         type=lambda x: int(x) if x!='cpu' else x
     )
+    parser.add_argument(
+        "--batch_size",
+        "-b",
+        help="Number of examples to process at a time.",
+        default=32,
+        type=int,
+    )
     return parser
 
 def dataset_generator(libris_split_dir: str) -> Generator[Tuple[str, str], None, None]:
@@ -64,6 +73,17 @@ def dataset_generator(libris_split_dir: str) -> Generator[Tuple[str, str], None,
 def unzip(zipped_list):
     return list(zip(*zipped_list))
 
+def dataloader(dataset: Dataset, batch_size: int) -> Generator[Dataset, None, None]:
+    indices = range(len(dataset))
+    for start_index in range(0, len(dataset), step=batch_size):
+        end_index = start_index+batch_size
+        yield dataset.select(indices[start_index:end_index])
+
+def get_audio_dataset(recordings: List[str]) -> Dataset:
+    ds = Dataset.from_dict({'audio': recordings})
+    ds = ds.cast_column('audio', Audio(sampling_rate=16_000))
+    return ds
+
 def transcribe_librispeech(args) -> int:
     """
     
@@ -72,16 +92,20 @@ def transcribe_librispeech(args) -> int:
     print("Reading dataset...")
     example_tuples = [example for example in dataset_generator(librispeech_directory)]
     recordings, transcriptions = unzip(example_tuples)
+    ds = get_audio_dataset(recordings)
 
-    print("Transcribing...")
+    print(f"Loading ASR pipeline for {args.model}...")
     pipe = pipeline(
         "automatic-speech-recognition",
         args.model,
         device=args.device
     )
-    for recording in tqdm(recordings):
-        pipe_output = pipe(recording)
-    hypotheses = [output['text'] for output in pipe_output]
+    print("Transcribing...")
+    hypotheses = []
+    num_batches = math.ceil(len(ds)/args.batch_size)
+    for batch in tqdm(dataloader(ds, args.batch_size), total=num_batches):
+        pipe_output = pipe(batch)
+        hypotheses.extend(output['text'] for output in pipe_output)
 
     df = pd.DataFrame({'reference': transcriptions, 'hypothesis': hypotheses})
 
