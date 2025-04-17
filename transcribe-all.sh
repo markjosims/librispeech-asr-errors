605e9a74edfd14627be1504a8e54807eafb0084a
Mark Simmons <mjsimmons@ucsd.edu>
2025-04-17 11:08:23 -0700

    write shell script for transcribing

---
+++ transcribe-all.sh
@@ -0,0 +1,8 @@
+LIBRISPEECH=~/datasets/LibriSpeech
+W2V2=facebook/wav2vec2-large-960h-lv60
+HuBERT=facebook/hubert-xlarge-ls960-ft
+WHISPER=openai/whisper-large-v2
+for split in dev-clean test-clean; do
+    for model in $W2V2 $HuBERT $WHISPER; do
+        python transcribe_librispeech.py -m $MODEL -s $split
+done
